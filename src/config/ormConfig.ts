2f2230c9128aef209ca389ab4e7bb1c7546c1255
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-20 15:05:27 -0300

    Add CarSchema in ormConfig

--- src/config/ormConfig.ts
+++ src/config/ormConfig.ts
@@ -1,11 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { BaseSchema } from 'src/common/infraestructure/baseSchema';
+import { CarSchema } from 'src/module/cars/infraestructure/car.schema';
 import { UserSchema } from 'src/module/user/infraestructure/user.schema';
 
 
 export const typeormConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: './data/database.db',
-  entities: [BaseSchema, UserSchema],
+  entities: [BaseSchema, UserSchema, CarSchema],
   synchronize: true,
 };
