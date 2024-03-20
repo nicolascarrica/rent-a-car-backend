672a36d2abb3861311f82a37ddf732930921f0a7
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-20 15:08:29 -0300

    Add CarModule in imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { UserModule } from "./module/user/user.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeormConfig } from "./config/ormConfig";
+import { CarModule } from "./module/cars/car.module";
 
 @Module({
   imports: [
     UserModule,
+    CarModule,
     TypeOrmModule.forRoot(typeormConfig)
   ],
   controllers: [],
