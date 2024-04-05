131a7378bfc9f6b25ec53bf2ef888b648737aac8
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-04-05 14:43:53 -0300

    Add ReservationSchema

--- src/config/ormConfig.ts
+++ src/config/ormConfig.ts
@@ -1,12 +1,13 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { BaseSchema } from 'src/common/infraestructure/baseSchema';
 import { CarSchema } from 'src/module/cars/infraestructure/car.schema';
+import { ReservationSchema } from 'src/module/reservations/infraestructure/reservation.schema';
 import { UserSchema } from 'src/module/user/infraestructure/user.schema';
 
 
 export const typeormConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: './data/database.db',
-  entities: [BaseSchema, UserSchema, CarSchema],
+  entities: [BaseSchema, UserSchema, CarSchema, ReservationSchema],
   synchronize: true,
 };
