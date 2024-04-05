77df0a13f52fae9ba761b834a415c04b5ccf4f3c
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-04-05 14:43:35 -0300

    Add Reservation module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { UserModule } from "./module/user/user.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeormConfig } from "./config/ormConfig";
 import { CarModule } from "./module/cars/car.module";
+import { ReservationModule } from "./module/reservations/reservation.module";
 
 @Module({
   imports: [
     UserModule,
     CarModule,
+    ReservationModule,
     TypeOrmModule.forRoot(typeormConfig)
   ],
   controllers: [],
