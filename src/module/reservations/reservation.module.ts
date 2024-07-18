07de2d01258d61c0b648fedb8bc38018fc4c10ca
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-07-18 16:40:10 -0300

    Update imports module

--- src/module/reservations/reservation.module.ts
+++ src/module/reservations/reservation.module.ts
@@ -1,14 +1,20 @@
 import { Module } from "@nestjs/common";
-import { ReservationService } from "./application/reservation.service";
-import { ReservationRepository } from "./infraestructure/reservation.respository";
-import { ReservationController } from "./interface/reservation.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ReservationService } from "./application/reservation.service";; // Corrige el nombre del archivo aquí
+import { ReservationController } from "./interface/reservation.controller";
 import { ReservationSchema } from "./infraestructure/reservation.schema";
+import { CarModule } from "../cars/car.module";
+import { UserModule } from "../user/user.module";
+import { ReservationMapper } from "./interface/reservation.mapper"; // Corregido si estaba mal
+import { ReservationRepository } from "./infraestructure/reservation.respository";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([ReservationSchema])],
-    controllers: [ReservationController],
-    providers: [ReservationService, ReservationRepository],
+  imports: [
+    TypeOrmModule.forFeature([ReservationSchema]),
+    CarModule,
+    UserModule,
+  ],
+  controllers: [ReservationController],
+  providers: [ReservationService, ReservationRepository, ReservationMapper],
 })
-
 export class ReservationModule {}
