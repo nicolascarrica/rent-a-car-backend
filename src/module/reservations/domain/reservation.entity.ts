bb766b4e46f92aa304061187d0e95643de5ec9ec
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-07-18 16:44:46 -0300

    Update Entity

--- src/module/reservations/domain/reservation.entity.ts
+++ src/module/reservations/domain/reservation.entity.ts
@@ -1,14 +1,47 @@
-import { BaseEntity } from "src/common/domain/base.entity";
+import { BaseEntity, BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Car } from "src/module/cars/domain/car.entity";
 import { User } from "src/module/user/domain/user.entity";
 
+@Entity()
 export class Reservation extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
   startDate: Date;
+
+  @Column()
   endDate: Date;
+
+  @Column()
   pricePerDay: number;
+
+  @Column()
+  totalDays: number;
+
+  @Column()
   totalPrice: number;
+
+  @Column()
   paymentMethod: string;
+
+  @Column()
   statusId: boolean;
+
+  @ManyToOne(() => Car, car => car.reservations)
   car: Car;
+
+  @ManyToOne(() => User, user => user.reservations)
   user: User;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  calculateTotals() {
+    if (this.startDate && this.endDate && this.pricePerDay) {
+      const start = new Date(this.startDate);
+      const end = new Date(this.endDate);
+      const diffTime = Math.abs(end.getTime() - start.getTime());
+      this.totalDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      this.totalPrice = this.totalDays * this.pricePerDay;
+    }
+  }
 }
