7b8a50bb2b765e4b16d84608eaae492b6d2182b8
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-07-20 12:17:31 -0300

    Update User entity

--- src/module/user/domain/user.entity.ts
+++ src/module/user/domain/user.entity.ts
@@ -1,15 +1,43 @@
+
 import { BaseEntity } from "src/common/domain/base.entity";
 import { Reservation } from "src/module/reservations/domain/reservation.entity";
+import { Entity, Column, OneToMany } from "typeorm";
 
+@Entity()
 export class User extends BaseEntity {
+  @Column()
   public name: string;
+
+  @Column()
   public lastName: string;
-  public docType: string;
-  public docNumber: string;
-  public nationality: string;
-  public address: string;
-  public phone: string;
+
+  @Column()
   public email: string;
+
+  @Column()
+  public phone: string;
+
+  @Column()
+  public docType: string;
+
+  @Column()
+  public docNumber: string;
+
+  @Column()
+  public nationality: string;
+
+  @Column()
+  public address: string;
+
+  @Column()
   public birthDate: Date;
+
+  @OneToMany(() => Reservation, reservation => reservation.user)
   public reservations: Reservation[];
+
+  @Column({ nullable: true })
+  public createdAt: Date;
+
+  @Column({ nullable: true })
+  public updatedAt: Date;
 }
