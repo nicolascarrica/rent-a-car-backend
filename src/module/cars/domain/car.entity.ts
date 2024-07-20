d7117dea8d596a7fc9e1481342863ab4c4cfef91
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-07-20 12:16:34 -0300

    Update Car entity

--- src/module/cars/domain/car.entity.ts
+++ src/module/cars/domain/car.entity.ts
@@ -1,16 +1,40 @@
+
 import { BaseEntity } from "src/common/domain/base.entity";
 import { Reservation } from "src/module/reservations/domain/reservation.entity";
+import { Entity, Column, OneToMany } from "typeorm";
 
+@Entity()
 export class Car extends BaseEntity {
+  @Column()
   public brand: string;
+
+  @Column()
   public model: string;
+
+  @Column()
   public color: string;
+
+  @Column()
   public kms: number;
+
+  @Column()
   public year: number;
+
+  @Column()
   public img: string;
+
+  @Column()
   public price: number;
+
+  @Column()
   public transmission: string;
+
+  @Column()
   public airConditioning: boolean;
+
+  @OneToMany(() => Reservation, reservation => reservation.car)
   public reservations: Reservation[];
+
+  @Column({ nullable: true })
   public deleteAt: Date;
 }
